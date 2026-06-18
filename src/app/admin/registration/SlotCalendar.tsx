54e26830822382e0e345a2616cb44c18742940ee
Filip Nowakowski <f.nowakowski2000@gmail.com>
2026-06-18 12:57:42 +0200

    feat(registration): enhance SlotCalendar with availability indicators

--- src/app/admin/registration/SlotCalendar.tsx
+++ src/app/admin/registration/SlotCalendar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Registration, Slot } from "@/prisma/generated";
+import { Stack } from "@mantine/core";
 import { Indicator } from "@mantine/core";
 import { DatePicker, DatePickerProps } from "@mantine/dates";
 import dayjs from "dayjs";
@@ -8,19 +9,30 @@ import dayjs from "dayjs";
 const renderDay = (slots: (Slot & { registrations: Registration[] })[]) => {
 	const render: DatePickerProps["renderDay"] = (date) => {
 		const day = dayjs(date).date();
-		const registrationsCount = slots
-			.filter((slot) => dayjs(slot.date).isSame(date, "day"))
-			.map((slot) => slot.registrations.length)
-			.reduce((acc, curr) => acc + curr, 0);
+		const daySlots = slots.filter((slot) =>
+			dayjs(slot.date).isSame(date, "day"),
+		);
+
+		const registeredSlots = daySlots.filter(
+			(slot) => slot.registrations.length > 0,
+		);
+		const isFull = registeredSlots.length === daySlots.length;
+		const color = isFull ? "green" : "yellow";
+		const isPast = dayjs(date).isBefore(dayjs(), "day");
 
 		return (
 			<Indicator
 				size={16}
-				label={registrationsCount}
-				processing
-				color="green"
+				position="top-end"
 				offset={-7}
-				disabled={registrationsCount === 0}
+				color={color}
+				label={
+					daySlots.length > 0
+						? `${registeredSlots.length}/${daySlots.length}`
+						: undefined
+				}
+				disabled={daySlots.length === 0}
+				processing={!isPast}
 			>
 				<div>{day}</div>
 			</Indicator>
@@ -37,13 +49,15 @@ export type SlotCalendarProps = {
 
 export function SlotCalendar(props: SlotCalendarProps) {
 	return (
-		<DatePicker
-			type="multiple"
-			value={props.value}
-			onChange={props.onChange}
-			maxLevel="month"
-			firstDayOfWeek={1}
-			renderDay={renderDay(props.slots)}
-		/>
+		<Stack align="center" gap="sm">
+			<DatePicker
+				type="multiple"
+				value={props.value}
+				onChange={props.onChange}
+				maxLevel="month"
+				firstDayOfWeek={1}
+				renderDay={renderDay(props.slots)}
+			/>
+		</Stack>
 	);
 }
